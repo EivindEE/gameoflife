63093b6722e17bce9a7e1c91ccc4f0a6a2f65e34
EivindEE <eivind.elseth@gmail.com>
2013-02-07 15:19:56 +0100

    Added propper test

--- src/test/java/edu/uib/gol/CellTest.java
+++ src/test/java/edu/uib/gol/CellTest.java
@@ -1,14 +1,14 @@
 package edu.uib.gol;
 
 import static org.junit.Assert.*;
-
+import edu.uib.gol.Cell;
 import org.junit.Test;
 
 public class CellTest {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testToString() {
+		assertTrue("Dead cells should print a single space", Cell.DEAD.toString().equals(" "));
 	}
 
 }
