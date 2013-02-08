2164c522d21b1ae55213cf0558d58066c3576556
Eivind Eidheim Elseth <eivind.elseth@gmail.com>
2013-02-08 12:21:47 +0100

    Added tests for building, size and getting and setting cells

--- src/test/java/edu/uib/gol/WorldAndFactoryTest.java
+++ src/test/java/edu/uib/gol/WorldAndFactoryTest.java
@@ -1,7 +1,9 @@
 package edu.uib.gol;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,15 +11,55 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration("classpath:test-context.xml")
-public class WorldTest {
-	@Autowired
-	World emptyWorld;
+public class WorldAndFactoryTest {
+	@Autowired 
+	WorldFactory worldFactory;
+	
+	World defaultWorld;
+	int defaultLength = 3;
+	int defaultHeight = 3;
+	@Before
+	public void setUp() {
+		defaultWorld = worldFactory.buildWorld(defaultLength, defaultHeight);
+	}
 	
 	@Test
-	public void testGetCellAt() {
+	public void testBuildWorld() {
+		World world = worldFactory.buildWorld(0,0);
+		assertTrue("The builder should return a World object ", world instanceof World);
+	}
+	
+	@Test
+	public void testWorldSize() {
+		assertEquals("The length of the universe should be the same as the length given",
+				defaultLength, defaultWorld.getWidth());
+		assertEquals("The height of the universe should be the same as the height given",
+				defaultHeight, defaultWorld.getHeight());
+		int newHeight = 10, 
+			newLength = 11;
+		World newWorld = worldFactory.buildWorld(newLength, newHeight);
+		assertEquals("The length of the universe should be the same as the length given",
+				newLength, newWorld.getWidth());
+		assertEquals("The height of the universe should be the same as the height given",
+				newHeight, newWorld.getHeight());
+	}
+	
+	@Test
+	public void testInitialState() {
+		// A new world should contain only Cell.DEAD
+		for( int i = 0; i < defaultLength; i++) {
+			for (int j = 0; j < defaultHeight; j++) {
+				assertEquals("Each cell in a default world should be dead", Cell.DEAD, defaultWorld.getCellAt(i, j));
+			}
+		}
+	}
+	
+	@Test
+	public void testGetAndSetCellAt() {
+		
 		int x = 0,
 			y = 0;
-		assertEquals("The cell at 0,0 should be dead(Cell.DEAD) in an empty map", Cell.DEAD, emptyWorld.getCellAt(x,y));
+		assertEquals("The cell at 0,0 should be dead(Cell.DEAD) in an empty map", Cell.DEAD, defaultWorld.getCellAt(x,y));
+		defaultWorld.setCellAt(x,y);
 	}
-
 }
