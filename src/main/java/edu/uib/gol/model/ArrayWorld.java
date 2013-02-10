608866276d3e5b318c9dd00dc3ff2ecf21f5e0db
Eivind Eidheim Elseth <eivind.elseth@gmail.com>
2013-02-10 18:01:47 +0100

    Changed ArrayWorld to a toroidal implementation

--- src/main/java/edu/uib/gol/model/ArrayWorld.java
+++ src/main/java/edu/uib/gol/model/ArrayWorld.java
@@ -2,6 +2,8 @@ package edu.uib.gol.model;
 
 import java.util.Arrays;
 
+import org.apache.commons.lang.Validate;
+
 
 /**
  * 
@@ -47,16 +49,28 @@ public class ArrayWorld implements World{
 	}
 	
 	public int numberOfAdjacentLivingCells(int x, int y) {
+		Validate.isTrue(x >= 0, "x must be >= 0");
+		Validate.isTrue(x < this.getWidth(), "x must be less than width of world");
+		Validate.isTrue(y>=0, "y must be >= 0");
+		Validate.isTrue(y < this.getHeight(), "y must be less than heigt of world");
 		int numberOfLivingCells = 0;
-		int xStart, xEnd, yStart, yEnd;
-		xStart = x == 0 ? 0 : -1;
-		yStart = y == 0 ? 0 : -1;
-		xEnd = x >= cells.length - 1? 0 : 1;
-		yEnd = y >= cells[0].length - 1? 0 : 1;
-		for (int i = xStart; i <= xEnd; i++) {
-			for (int j = yStart; j <= yEnd; j++) {
+		int xPos, yPos;
+		for (int i = -1; i <= 1; i++) {
+			for (int j = -1; j <= 1; j++) {
 				if (!(i == 0 && j == 0)) {
-					numberOfLivingCells += this.getCellAt(x + i, y + j) == Cell.LIVING ? 1 : 0;
+					xPos = x + i;
+					yPos = y + j;
+					if ( xPos >= cells.length) {
+						xPos = 0;
+					} else if (xPos < 0) {
+						xPos = cells.length - 1;
+					}
+					if ( yPos >= cells[xPos].length) {
+						yPos = 0;
+					} else if (yPos < 0) {
+						yPos = cells[xPos].length - 1;
+					}
+					numberOfLivingCells += this.getCellAt(xPos, yPos) == Cell.LIVING ? 1 : 0;
 				}
 			}
 		}
