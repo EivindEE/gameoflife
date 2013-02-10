840bbb1fee92129d4236b86f81a384ec37cae4c8
Eivind Eidheim Elseth <eivind.elseth@gmail.com>
2013-02-10 15:59:28 +0100

    Updated main to use the new GUIWorldViewer

--- src/main/java/edu/uib/gol/Main.java
+++ src/main/java/edu/uib/gol/Main.java
@@ -8,6 +8,7 @@ import edu.uib.gol.model.Universe;
 import edu.uib.gol.model.World;
 import edu.uib.gol.model.factory.UniverseFactory;
 import edu.uib.gol.model.factory.WorldFactory;
+import edu.uib.gol.view.GUIWorldViewer;
 import edu.uib.gol.view.WorldViewer;
 
 public class Main {
@@ -17,21 +18,24 @@ public class Main {
         ApplicationContext context = new ClassPathXmlApplicationContext("main-context.xml");
         WorldFactory worldFactory = context.getBean(WorldFactory.class);
         UniverseFactory universeFactory = context.getBean(UniverseFactory.class);
-        WorldViewer worldViewer = context.getBean(WorldViewer.class);
+        
         Cell[][] cells = new Cell[][] {
     			{Cell.DEAD, Cell.DEAD, Cell.DEAD, Cell.DEAD, Cell.DEAD, Cell.DEAD},
     			{Cell.DEAD, Cell.DEAD, Cell.DEAD, Cell.DEAD, Cell.DEAD, Cell.DEAD},
     			{Cell.DEAD, Cell.DEAD, Cell.LIVING, Cell.LIVING, Cell.LIVING, Cell.DEAD},
     			{Cell.DEAD, Cell.LIVING, Cell.LIVING, Cell.LIVING, Cell.DEAD, Cell.DEAD},
-    			{Cell.DEAD, Cell.DEAD, Cell.LIVING, Cell.DEAD, Cell.DEAD, Cell.DEAD},
-    			{Cell.DEAD, Cell.DEAD, Cell.DEAD, Cell.LIVING, Cell.DEAD, Cell.DEAD}
+    			{Cell.DEAD, Cell.DEAD, Cell.DEAD, Cell.DEAD, Cell.DEAD, Cell.DEAD},
+    			{Cell.DEAD, Cell.DEAD, Cell.DEAD, Cell.DEAD, Cell.DEAD, Cell.DEAD}
     	};
         World world = worldFactory.createWorld(cells);
         Universe universe = universeFactory.createUniverse(worldFactory, world);
-        for (int i = 0; i < 100; i++) {
+        GUIWorldViewer worldViewer = new GUIWorldViewer(universe);
+		worldViewer.init();
+        while(true) {
+        	universe.tick();
         	worldViewer.drawWorld();
-        	Thread.sleep(1000);
-		}
+        	Thread.sleep(500);
+        }
+        
 	}
-
 }
