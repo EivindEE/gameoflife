e155676d21e9178e58004ce334b25516176523b6
Eivind Eidheim Elseth <eivind.elseth@gmail.com>
2013-02-11 15:01:14 +0100

    Added component annotation

--- src/main/java/edu/uib/gol/model/factory/ToroidalArrayWorldFactory.java
+++ src/main/java/edu/uib/gol/model/factory/ToroidalArrayWorldFactory.java
@@ -1,9 +1,12 @@
 package edu.uib.gol.model.factory;
 
+import org.springframework.stereotype.Component;
+
 import edu.uib.gol.model.Cell;
 import edu.uib.gol.model.ToroidalArrayWorld;
 import edu.uib.gol.model.World;
 
+@Component
 public class ToroidalArrayWorldFactory extends ArrayWorldFactory {
 	@Override
 	public World createWorld(Cell[][] cells) {
