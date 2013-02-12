fe38186825e3051f1c4b72b547fef59f50a248ad
Eivind Eidheim Elseth <eivind.elseth@gmail.com>
2013-02-12 10:09:54 +0100

    changed from jpanel to graphics draw implementation

--- src/main/java/edu/uib/gol/view/GUIWorldViewer.java
+++ src/main/java/edu/uib/gol/view/GUIWorldViewer.java
@@ -3,11 +3,15 @@ package edu.uib.gol.view;
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
 import java.awt.GridLayout;
 import java.awt.Toolkit;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+
+import org.apache.commons.lang.Validate;
 
 import edu.uib.gol.model.Cell;
 import edu.uib.gol.model.Universe;
@@ -21,48 +25,52 @@ public class GUIWorldViewer extends JFrame implements WorldViewer  {
 	private static final long serialVersionUID = 1L;
 
 	private Universe universe;
-	private JPanel[][] grid;
-	private int cellXY = 50;
-	private Dimension cellDimension = new Dimension(cellXY,cellXY);
-	
+
+	private int cellXY;
+//	private JPanel[][] grid;
+
 	public GUIWorldViewer (Universe universe) {
+		Validate.notNull(universe, "Universe cannot be null");
 		this.universe = universe;
 		World w = this.universe.getWorld();
 		int width = w.getWidth();
 		int height = w.getHeight();
-		this.grid = new JPanel[width][height];
+		//		this.grid = new JPanel[width][height];
 		this.setLayout(new GridLayout(width, height));
 		this.setResizable(false);
 	}
-	
+
 	public void init() {
 		Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
-		while (cellDimension.getWidth() * grid.length > screenSize.getWidth()
-				||
-				cellDimension.getHeight() * grid[0].length > screenSize.getHeight()){
-			cellXY--;
-			cellDimension = new Dimension(cellXY, cellXY);
-		}
-		this.setSize(new Dimension((int) cellDimension.getHeight() * grid[0].length, (int) cellDimension.getWidth() * grid.length));
-		for(int i = 0; i < grid.length; i++) {
-			for (int j = 0; j < grid[i].length; j++) {
-				grid[i][j] = new JPanel(new BorderLayout());
-				grid[i][j].setSize(cellDimension);
-				grid[i][j].setBackground(universe.getWorld().getCellAt(i, j) == Cell.LIVING ? Color.GREEN:  Color.BLACK);
-				this.add(grid[i][j]);
-			}
-		}
-		
+		cellXY = Math.min((screenSize.width / universe.getWorld().getWidth()), (screenSize.height / universe.getWorld().getHeight()));
+		Dimension cellDimension = new Dimension(cellXY,cellXY);
+		this.setSize(new Dimension(cellXY * universe.getWorld().getHeight(), cellXY * universe.getWorld().getWidth()));
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		this.setVisible(true);
 	}
-	
+
 	public void drawWorld() {
-		for(int i = 0; i < grid.length; i++) {
-			for (int j = 0; j < grid[i].length; j++) {
-				grid[i][j].setBackground(universe.getWorld().getCellAt(i, j) == Cell.LIVING ? Color.GREEN:  Color.BLACK);
+		Graphics2D g = (Graphics2D) this.getGraphics();
+		World world = universe.getWorld();
+		int width = world.getWidth();
+		int height = world.getHeight();
+		for(int i = 0; i < width; i++) {
+			for (int j = 0; j < height; j++) {
+				Color color = world.getCellAt(i, j) == Cell.LIVING ? Color.DARK_GRAY:  Color.CYAN;
+				drawCell(color, i, j, g);
 			}
 		}
 	}
 
+	private void drawCell(Color color, int x, int y, Graphics2D g) {
+		if (g == null) {
+			g = (Graphics2D) getGraphics();
+		}
+		if (g != null) {
+			g.setPaint(color);
+			g.fillRect(x * cellXY, y * cellXY, cellXY, cellXY);
+		}
+	}
+	
+	
 }
